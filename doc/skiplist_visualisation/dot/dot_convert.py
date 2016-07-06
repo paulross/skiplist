2ca7112350d2457d169736384cc5a5ec1316b04f
Paul Ross <apaulross@gmail.com>
2016-07-06 12:44:53 +0100

    Visualisations of nodes are now ordered top to bottom.

---
+++ doc/skiplist_visualisation/dot/dot_convert.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+import os
+import subprocess
+import sys
+
+def main():
+    this_file = os.path.join(os.getcwd(), __file__)
+    for root, dirs, files in os.walk(os.path.dirname(this_file)):
+        for file in files:
+            fname, ext = os.path.splitext(file)
+            if ext == '.dot':
+                print('Converting: %s' % file)
+                fp_in = os.path.join(root, fname + ext)
+                # dot -odoc_insert_remove.png -Tpng doc_insert_remove.dot
+                for typ in ('svg', 'png'):
+                    cmds = ['dot', '-o%s' % os.path.join(root, fname + '.' + typ), '-T%s' % typ, fp_in]
+#                     print(cmds)
+                    subprocess.Popen(cmds)
+    return 0
+    
+if __name__ == '__main__':
+    sys.exit(main())    
+
+
