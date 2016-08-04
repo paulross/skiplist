ee657bef659717aaa40225fa84c3a5a7d630c656
Paul Ross <apaulross@gmail.com>
2016-08-04 19:52:04 +0100

    Moves setup.py/.cfg to project root.

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
 #!/bin/env python
-#
-# $HeadURL$
-
-# try:
-#     from ahl.pkgutils.setuptools import setup
-# except ImportError:
-#     print "AHL Package Utilities are not available. Please run \"easy_install ahl.pkgutils\""
-#     import sys
-#     sys.exit(1)
-
-# import sys
 import os
-# from distutils import log
 from distutils.core import setup, Extension
-
-# from ahl.pkgutils.setuptools.extension import NumpyExtension
-
-# from ahl.pkgutils.setuptools.command.build_ext import build_ext  # as orig_build_ext
 
 # Makefile:
 # CFLAGS = -Wall -Wextra
@@ -32,8 +16,8 @@ else:
 
 cSkipList = Extension("cSkipList",
                              sources=[
-                                      'cSkipList.cpp',
-                                      'SkipList.cpp',
+                                      'src/cpp/cSkipList.cpp',
+                                      'src/cpp/SkipList.cpp',
                                       ],
                              include_dirs=['.', ],
                              library_dirs=[os.getcwd(), ],
