afcb30c14ae1bfcc9d567cefbd2e4a2326e45205
Paul Ross <apaulross@gmail.com>
2016-10-02 12:22:48 +0100

    Adds initial tests for SortedMap.

---
+++ tests/unit/test_SortedMap.py
@@ -0,0 +1,53 @@
+import itertools
+
+import pytest
+
+import cSkipList
+from . import seed_tree
+
+from .compat_23 import int_type, math_nan, example_int_value, example_int_seq
+
+@pytest.mark.parametrize('typ', [int_type, float, bytes])
+def test_ctor(typ):
+    assert cSkipList.PyStdMap(typ) is not None
+
+def test_ctor_raises_no_type():
+    with pytest.raises(TypeError) as err:
+        cSkipList.PyStdMap()
+    assert err.value.args[0] == "Required argument 'value_type' (pos 1) not found"
+
+def test_ctor_raises_not_a_type():
+    with pytest.raises(ValueError) as err:
+        cSkipList.PyStdMap('')
+    assert err.value.args[0] == \
+        'Argument to __init__ must be type object not "str"'
+
+@pytest.mark.parametrize('typ, msg', [
+                                 (complex, 'complex'),
+                                 (tuple, 'tuple'),
+                                 (list, 'list'),
+                                 (dict, 'dict'),
+                                 ])
+def test_ctor_raises_wrong_type(typ, msg):
+    with pytest.raises(ValueError) as err:
+        cSkipList.PyStdMap(typ)
+    assert err.value.args[0] == \
+        'Argument to __init__ must be long, float or bytes, not "%s"' % msg
+
+@pytest.mark.parametrize('typ', [int_type, float, bytes])
+def test_size_zero(typ):
+    sm = cSkipList.PyStdMap(typ)
+    assert sm.size() == 0
+    
+@pytest.mark.parametrize('typ', [int_type, float, bytes])
+def test_max_size_greater_than_zero(typ):
+    sm = cSkipList.PyStdMap(typ)
+    max_size = sm.max_size()
+    assert max_size.bit_length() > 32
+    
+# @pytest.mark.parametrize('typ,value',
+#                          [(int_type, example_int_value), (float, 8.0), (bytes, b'abc')])
+# def test_single_insert(typ, value):
+#     sm = cSkipList.PyStdMap(typ)
+#     assert sm.insert(value) is None
+#     assert sm.size() == 1
