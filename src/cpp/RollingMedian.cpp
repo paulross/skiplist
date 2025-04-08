8f881ccaa2edcc6eb75ea481dc7c1364ba0daa76
Paul Ross <apaulross@gmail.com>
2025-04-08 13:57:59 +0100

    Remove doc from implementation in RollingMedian.cpp.

--- src/cpp/RollingMedian.cpp
+++ src/cpp/RollingMedian.cpp
@@ -9,27 +9,10 @@ namespace OrderedStructs {
      */
     namespace RollingMedian {
 
-/**
- * Returns the size of the destination array for a rolling median on an array
- * of count values with a window of win_length and a destination stride.
- *
- * @param count Number of input values.
- * @param win_length Window length.
- * @return Number of destination values.
- */
         size_t dest_count(size_t count, size_t win_length) {
             return 1 + count - win_length;
         }
 
-/**
- * Returns the size of the destination array for a rolling median on an array
- * of count values with a window of win_length and a destination stride.
- *
- * @param count Number of input values.
- * @param win_length Window length.
- * @param dest_stride The destination stride given a 2D array.
- * @return Size of destination array.
- */
         size_t dest_size(size_t count,
                          size_t win_length,
                          size_t dest_stride) {
