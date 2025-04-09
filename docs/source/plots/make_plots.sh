a809efbaef9818296597982d85133ce65382bbc7
Paul Ross <apaulross@gmail.com>
2025-04-09 10:04:27 +0100

    Simplify make_plots.sh

--- docs/source/plots/make_plots.sh
+++ docs/source/plots/make_plots.sh
@@ -1,16 +1,3 @@
 echo "Creating plots..."
-gnuplot -p biased_coin_effect.plt
-gnuplot -p perf_at_has.plt
-gnuplot -p perf_height_size.plt
-gnuplot -p perf_ins_rem_mid.plt
-gnuplot -p perf_roll_med_odd_index_wins.plt
-gnuplot -p perf_size_of.plt
-gnuplot -p perf_index.plt
-gnuplot -p perf_cpp_threaded_fixed_size.plt
-gnuplot -p perf_cpp_threaded_vari_size.plt
-gnuplot -p perf_cpp_threaded_vs_single.plt
-gnuplot -p perf_roll_median_shared_memory.plt
-gnuplot -p perf_rolling_median_shared_memory_ratio.plt
-gnuplot -p perf_rolling_median_shared_memory_ratio_by_col.plt
-gnuplot -p perf_cpp_threaded_sim_rolling_median.plt
+gnuplot -p *.plt
 echo "All plots done"
