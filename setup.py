a64ce6a2aa6c6199cf4e964c97395653067f1532
Paul Ross <apaulross@gmail.com>
2026-01-21 17:41:52 +0000

    Add doc references to python 3.14 support.

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
     ],
     test_suite='tests',
     tests_require=test_requirements,
