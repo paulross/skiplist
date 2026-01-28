771dad6979f750fab6ccc8ae511023c56eef3d19
Paul Ross <apaulross@gmail.com>
2026-01-28 15:52:31 +0000

    Move rolling median performance tests to test_performance.cpp. Add rolling median string test in Python.

--- src/cpp/test/test_rolling_median.cpp
+++ src/cpp/test/test_rolling_median.cpp
@@ -151,203 +151,16 @@ int perf_roll_med_odd_index() {
     return result;
 }
 
-/**
- * @brief Test the performance of a simple rolling median into a dynamic array of doubles given by
- * \c OrderedStructs::RollingMedian::dest_size. Odd length window.
- *
- * @return Zero on success, non-zero on failure.
- */
-int perf_roll_med_odd_index_wins(TestResultS &test_results) {
-    const size_t COUNT = 1000000;
-    const int REPEAT = 5;
-    const int DEST_STRIDE = 1;
-    double *src = new double[COUNT];
-    int result = 0;
-
-    std::ostringstream title;
-    title << __FUNCTION__ << "[" << COUNT << "]";
-    TestResult test_result(title.str());
-//    std::cout << "Running: " << title.str() << std::endl;
-
-    // Create source data
-    for (size_t i = 0; i < COUNT; ++i) {
-        src[i] = 2.0 * i;
-    }
-    // Loop over this data for various window sizes from 1 to 524288
-    for (size_t win = 1; win < COUNT; win *= 2) {
-        for (int i = 0; i < REPEAT; ++i) {
-            size_t dest_size = OrderedStructs::RollingMedian::dest_size(COUNT, win, DEST_STRIDE);
-            double *dest = new double[dest_size];
-            srand(1);
-//            time_t start = clock();
-            ExecClock exec_clock;
-            result |= OrderedStructs::RollingMedian::odd_index(src, 1, COUNT,
-                                                               win, dest, DEST_STRIDE);
-//            double exec = 1e6 * (clock() - start) / (double) CLOCKS_PER_SEC;
-            double exec_time = exec_clock.seconds();
-//            std::cout << std::setw(FUNCTION_WIDTH) << __FUNCTION__ << "():";
-//            std::cout << " vectors length: " << std::setw(8) << COUNT;
-//            std::cout << " window width: " << std::setw(6) << win;
-//            std::cout << " time: ";
-//            std::cout << exec_time * 1e3;
-//            std::cout << " (ms)";
-//            std::cout << std::endl;
-            delete[] dest;
-            test_result.execTimeAdd(0, exec_time, 1, win);
-        }
-    }
-    delete [] src;
-    test_results.push_back(test_result);
-    return result;
-}
-
-/**
- * @brief Test the performance of a simple rolling median using std::vector<double>.
- * Even length window.
- *
- * @return Zero on success, non-zero on failure.
- */
-int perf_roll_med_vector_style_even_win_length(TestResultS &test_results) {
-    const size_t COUNT = 1000000;
-    const int REPEAT = 5;
-    std::vector<double> src;
-    std::vector<double> dest;
-    int result = 0;
-
-    std::ostringstream title;
-    title << __FUNCTION__ << "[" << COUNT << "]";
-    TestResult test_result(title.str());
-//    std::cout << "Running: " << title.str() << std::endl;
-
-    // Create source data
-    for (size_t i = 0; i < COUNT; ++i) {
-        src.push_back(2.0 * i);
-    }
-    // Loop over this data for various window sizes from 2 to 524288
-    for (size_t win = 2; win < COUNT; win *= 2) {
-        for (int i = 0; i < REPEAT; ++i) {
-            srand(1);
-            //        time_t start = clock();
-            ExecClock exec_clock;
-            result |= OrderedStructs::RollingMedian::rolling_median(src, win, dest);
-            //        double exec = 1e6 * (clock() - start) / (double) CLOCKS_PER_SEC;
-            double exec_time = exec_clock.seconds();
-//            std::cout << std::setw(FUNCTION_WIDTH) << __FUNCTION__ << "():";
-//            std::cout << " vectors length: " << std::setw(8) << COUNT;
-//            std::cout << " window width: " << std::setw(6) << win;
-//            std::cout << " time: ";
-//            std::cout << exec_time * 1e3;
-//            std::cout << " (ms)";
-//            std::cout << std::endl;
-            test_result.execTimeAdd(0, exec_time, 1, win);
-        }
-    }
-    test_results.push_back(test_result);
-    return result;
-}
-
-/**
- * @brief Test the performance of a simple rolling median using std::vector<double>.
- * Odd length window.
- *
- * @return Zero on success, non-zero on failure.
- */
-int perf_roll_med_vector_style_odd_win_length(TestResultS &test_results) {
-    const size_t COUNT = 1000000;
-    const int REPEAT = 5;
-    std::vector<double> src;
-    std::vector<double> dest;
-    int result = 0;
-
-    std::ostringstream title;
-    title << __FUNCTION__ << "[" << COUNT << "]";
-    TestResult test_result(title.str());
-//    std::cout << "Running: " << title.str() << std::endl;
-
-    // Create source data
-    for (size_t i = 0; i < COUNT; ++i) {
-        src.push_back(2.0 * i);
-    }
-    // Loop over this data for various window sizes from 2 to 524288
-    for (size_t win = 2; win < COUNT; win *= 2) {
-        for (int i = 0; i < REPEAT; ++i) {
-            srand(1);
-//            time_t start = clock();
-            ExecClock exec_clock;
-            result |= OrderedStructs::RollingMedian::rolling_median(src, win + 1, dest);
-//            double exec = 1e6 * (clock() - start) / (double) CLOCKS_PER_SEC;
-            double exec_time = exec_clock.seconds();
-//            std::cout << std::setw(FUNCTION_WIDTH) << __FUNCTION__ << "():";
-//            std::cout << " vectors length: " << std::setw(8) << COUNT;
-//            std::cout << " window width: " << std::setw(6) << win;
-//            std::cout << " time: ";
-//            std::cout << exec_time * 1e3;
-//            std::cout << " (ms)";
-//            std::cout << std::endl;
-            test_result.execTimeAdd(0, exec_time, 1, win);
-        }
-    }
-    test_results.push_back(test_result);
-    return result;
-}
-
-/**
- * @brief Test the performance of a simple rolling median using std::vector<std::string>.
- * Even length window.
- *
- * @return Zero on success, non-zero on failure.
- */
-int perf_roll_med_vector_style_even_win_length_string(TestResultS &test_results) {
-    const size_t COUNT = 1000000 / 10;
-    int REPEAT = 5;
-    std::vector<std::string> src;
-    std::vector<std::string> dest;
-    int result = 0;
-
-    std::ostringstream title;
-    title << __FUNCTION__ << "[" << COUNT << "]";
-    TestResult test_result(title.str());
-//    std::cout << "Running: " << title.str() << std::endl;
-
-    // Create source data
-    for (size_t i = 0; i < COUNT; ++i) {
-        src.push_back(random_string(1024));
-    }
-    // Loop over this data for various window sizes from 2 to 524288
-    for (size_t win = 2; win < COUNT; win *= 2) {
-        for (int i = 0; i < REPEAT; ++i) {
-            srand(1);
-//        time_t start = clock();
-            ExecClock exec_clock;
-            result |= OrderedStructs::RollingMedian::rolling_median_lower_bound(src, win, dest);
-//        double exec = 1e6 * (clock() - start) / (double) CLOCKS_PER_SEC;
-            double exec_time = exec_clock.seconds();
-//            std::cout << std::setw(FUNCTION_WIDTH) << __FUNCTION__ << "():";
-//            std::cout << " vectors length: " << std::setw(8) << COUNT;
-//            std::cout << " window width: " << std::setw(6) << win;
-//            std::cout << " time: ";
-//            std::cout << exec_time * 1e3;
-//            std::cout << " (ms)";
-//            std::cout << std::endl;
-            test_result.execTimeAdd(0, exec_time, 1, win);
-        }
-    }
-    test_results.push_back(test_result);
-    return result;
-}
-
 /******************* END: Rolling Median Tests ************************/
 
 /**
- * @brief Test all rolling median tests.
+ * @brief Test all rolling median tests. Performance tests are in src/cpp/test/test_performance.cpp
  *
  * @return Zero on success, non-zero on failure.
  */
 int test_rolling_median_all() {
     int result = 0;
 
-    TestResultS perf_test_results;
-    
     result |= print_result("test_roll_med_simple", test_roll_med_simple());
     result |= print_result("test_roll_med_even_win", test_roll_med_even_win());
     result |= print_result("test_roll_med_even_mean", test_roll_med_even_mean());
@@ -355,26 +168,7 @@ int test_rolling_median_all() {
     // integrity is very expensive for large data sets.
 #ifndef DEBUG
     result |= print_result("perf_roll_med_odd_index", perf_roll_med_odd_index());
-    result |= print_result(
-            "perf_roll_med_odd_index_wins",
-            perf_roll_med_odd_index_wins(perf_test_results)
-            );
-    result |= print_result(
-            "perf_roll_med_vector_style_even_win_length",
-            perf_roll_med_vector_style_even_win_length(perf_test_results)
-            );
-    result |= print_result(
-            "perf_roll_med_vector_style_odd_win_length",
-            perf_roll_med_vector_style_odd_win_length(perf_test_results)
-            );
-    result |= print_result(
-            "perf_roll_med_vector_style_even_win_length_string",
-            perf_roll_med_vector_style_even_win_length_string(perf_test_results)
-            );
 #endif
-    perf_test_results.dump_header(std::cout);
-    perf_test_results.dump_tests(std::cout);
-    perf_test_results.dump_tail(std::cout);
 
     return result;
 }
--- tests/benchmarks/test_benchmark_SkipList_rolling_median.py
+++ tests/benchmarks/test_benchmark_SkipList_rolling_median.py
@@ -1,7 +1,9 @@
 """
 Bench mark tests for Python rolling median. Typical usage:
-$ caffeinate pytest --benchmark-sort=fullname tests/benchmarks/test_benchmark_SkipList_floats.py --runslow --benchmark-autosave --benchmark-histogram
+$ caffeinate pytest tests/benchmarks/test_benchmark_SkipList_rolling_median.py --runslow --benchmark-sort=fullname --benchmark-autosave --benchmark-histogram
 """
+import random
+import string
 import typing
 
 import numpy as np
@@ -12,10 +14,10 @@ import orderedstructs
 
 # Make formatted strings so that the test name sorts nicely.
 # Typ. ('      1', ... '  524288',)
-ROLLING_MEDIAN_WINDOW_WIDTHS = tuple(f'{2 ** i:8d}' for i in range(0, 20, 1))
+ROLLING_MEDIAN_FLOAT_WINDOW_WIDTHS = tuple(f'{2 ** i:8d}' for i in range(0, 20, 1))
 
 
-def _test_rolling_median(vector: np.array, result: np.array, window_length: int):
+def _test_rolling_median_float(vector: np.array, result: np.array, window_length: int):
     skip_list = orderedstructs.SkipList(float)
     for i in range(len(vector)):
         value = vector[i]
@@ -29,9 +31,46 @@ def _test_rolling_median(vector: np.array, result: np.array, window_length: int)
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize('window_length', ROLLING_MEDIAN_WINDOW_WIDTHS)
-def test_rolling_median_by_window_length(benchmark, window_length):
+@pytest.mark.parametrize('window_length', ROLLING_MEDIAN_FLOAT_WINDOW_WIDTHS)
+def test_rolling_median_float_by_window_length(benchmark, window_length):
     length = 1000 ** 2
     vector = np.random.random(length)
     result = np.empty_like(vector)
-    benchmark(_test_rolling_median, vector, result, int(window_length))
+    benchmark(_test_rolling_median_float, vector, result, int(window_length))
+
+
+# Source - https://stackoverflow.com/a
+# Posted by Ignacio Vazquez-Abrams, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-28, License - CC BY-SA 4.0
+# Modified to use all printable characters.
+def random_bytes(length: int) -> bytes:
+    return ''.join(random.choices(string.printable, k=length)).encode('ascii')
+
+
+# Make formatted strings so that the test name sorts nicely.
+# Typ. ('      1', ... '  65536',)
+# Align with perf_roll_med_vector_style_even_win_length_string()
+ROLLING_MEDIAN_STR_WINDOW_WIDTHS = tuple(f'{2 ** i:8d}' for i in range(0, 17, 1))
+
+
+def _test_rolling_median_str(vector: typing.List[str], window_length: int):
+    skip_list = orderedstructs.SkipList(bytes)
+    result = []
+    for i in range(len(vector)):
+        value = vector[i]
+        skip_list.insert(value)
+        if i >= window_length - 1:
+            median = skip_list.at(window_length // 2)
+            result.append(median)
+            skip_list.remove(vector[i - window_length + 1])
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize('window_length', ROLLING_MEDIAN_STR_WINDOW_WIDTHS)
+def test_rolling_median_float_by_window_length(benchmark, window_length):
+    # Align with perf_roll_med_vector_style_even_win_length_string()
+    length = 100000
+    vector = []
+    for i in range(length):
+        vector.append(random_bytes(1024))
+    benchmark(_test_rolling_median_str, vector, int(window_length))
