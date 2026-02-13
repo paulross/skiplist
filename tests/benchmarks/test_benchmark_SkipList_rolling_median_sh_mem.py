3bbc0507384caca4207b9d249db6b16306d2825c
Paul Ross <apaulross@gmail.com>
2026-02-13 10:57:08 +0000

    Improve test_benchmark_SkipList_rolling_median_sh_mem.py. This takes about 6 hours per Python version with --runveryslow.

--- tests/benchmarks/test_benchmark_SkipList_rolling_median_sh_mem.py
+++ tests/benchmarks/test_benchmark_SkipList_rolling_median_sh_mem.py
@@ -1,5 +1,7 @@
 """
-Bench mark tests for Python rolling median with shared memory. Typical usage:
+Benchmark tests for Python rolling median with shared memory. Typical usage:
+
+pytest tests/benchmarks/test_benchmark_SkipList_rolling_median_sh_mem.py --runslow --runveryslow --benchmark-sort=name --benchmark-autosave --benchmark-histogram -v
 """
 import numpy as np
 
@@ -18,27 +20,44 @@ def _test_rolling_median_float(read_array: np.array, processes: int) -> np.array
 
 @pytest.mark.veryslow
 @pytest.mark.parametrize(
-    'rows, columns, processes',
-    (
-            # (1024, 16, 1,),
-            (1024 * 1024 * 8, 16, 1,),
-            (1024 * 1024 * 8, 16, 2,),
-            (1024 * 1024 * 8, 16, 4,),
-            (1024 * 1024 * 8, 16, 8,),
-            (1024 * 1024 * 8, 16, 16,),
-            (1024 * 128, 1024, 1,),
-            (1024 * 128, 1024, 2,),
-            (1024 * 128, 1024, 4,),
-            (1024 * 128, 1024, 8,),
-            (1024 * 128, 1024, 16,),
-            (1024 * 2, 64 * 1024, 1,),
-            (1024 * 2, 64 * 1024, 2,),
-            (1024 * 2, 64 * 1024, 4,),
-            (1024 * 2, 64 * 1024, 8,),
-            (1024 * 2, 64 * 1024, 16,),
-    ),
+    'processes',
+    ( 1, 2, 4, 8, 16,)
+    # ( 4,)
 )
-def test_rolling_median_sh_mem(benchmark, rows, columns, processes):
-    assert rows * columns == 128 * 1024
+def test_rolling_median_sh_mem_8388608_16(benchmark, processes):
+    rows = 1024 * 1024 * 8
+    columns = 16
+    # 134217728 values, 1073741824 bytes (1GB)
+    assert rows * columns == 2048 * 65536
+    read_array = np.random.random((rows, columns))
+    benchmark(_test_rolling_median_float, read_array, processes)
+
+
+@pytest.mark.veryslow
+@pytest.mark.parametrize(
+    'processes',
+    ( 1, 2, 4, 8, 16,)
+    # ( 4,)
+)
+def test_rolling_median_sh_mem_131072_1024(benchmark, processes):
+    rows = 1024 * 128
+    columns = 1024
+    # 134217728 values, 1073741824 bytes (1GB)
+    assert rows * columns == 2048 * 65536
+    read_array = np.random.random((rows, columns))
+    benchmark(_test_rolling_median_float, read_array, processes)
+
+
+@pytest.mark.veryslow
+@pytest.mark.parametrize(
+    'processes',
+    ( 1, 2, 4, 8, 16,)
+    # ( 4,)
+)
+def test_rolling_median_sh_mem_2048_65536(benchmark, processes):
+    rows = 1024 * 2
+    columns = 65536
+    # 134217728 values, 1073741824 bytes (1GB)
+    assert rows * columns == 2048 * 65536
     read_array = np.random.random((rows, columns))
     benchmark(_test_rolling_median_float, read_array, processes)
